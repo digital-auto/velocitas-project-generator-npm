006b76c1fb0eae6aa451bb08d8c0d6c5da73b973
Tam Thai Hoang Minh <tam.thaihoangminh@vn.bosch.com>
2024-11-04 18:16:37 +0700

    Init project

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,12 +13,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const GITHUB_API_URL = 'https://api.github.com/repos';
-export const PYTHON_TEMPLATE_URL = `${GITHUB_API_URL}/eclipse-velocitas/vehicle-app-python-template`;
+export const PYTHON_TEMPLATE_URL = `${GITHUB_API_URL}/digital-auto/vehicle-app-python-template`;
 
 export const CONTENT_ENCODINGS = { utf8: 'utf-8', base64: 'base64' };
 
 export const GIT_DATA_TYPES = { blob: 'blob', tree: 'tree', commit: 'commit' };
-export const GIT_DATA_MODES = {
+export const GIT_DATA_MODES = { 
     fileBlob: '100644',
     executableBlob: '100755',
     subdirectoryTree: '040000',
